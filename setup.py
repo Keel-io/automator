6a699881e042e5ebbd01b2edc62206389d65c700
Andrew Kelley <ak@keel.io>
2018-03-16 20:39:40 -0400

    Pushed to PyPi

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-import setuptools
-from automator.version import Version
+from setuptools import setup
 
-
-setuptools.setup(name='automator',
-                 version=Version('0.2.0').number,
-                 description='Data cleaning made easy',
-                 long_description=open('README.md').read().strip(),
-                 author='Keel',
-                 author_email='support@keel.io',
-                 url='https://github.com/keel-io/automator',
-                 py_modules=['automator'],
-                 install_requires=[],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='sql, data science, data cleaning',
-                 classifiers=['Data cleaning', 'Sql'])
+setup(
+    name="automator",
+    version="0.1.0",
+    packages=['automator'],
+    include_package_data=True,
+    install_requires=[
+        'psycopg2-binary'
+    ],
+)
